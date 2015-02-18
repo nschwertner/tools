d4f02e87e4b02d9036933caecfda9e2b1dd9fea8
amy6 <amy@motibox.com>
2015-02-17 21:57:11 -0500

    Initial commit

---
+++ tools/src/main/java/org/hspconsortium/DataLoad.java
@@ -0,0 +1,62 @@
+package org.hspconsortium;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.IOException;
+
+public class DataLoad {
+    private static final String STATIC_DATA_PATH = "org/hspconsortium/platform/sample/clinicaldata";
+    private static final String RESOURCE_URI = "http://localhost:8080/hsp-api";
+
+    public static void main(String[] args) throws Exception{
+
+        PathMatchingResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
+        org.springframework.core.io.Resource[] patientFileResources = resourceResolver.getResources(String.format("classpath:%s/*.fhir-bundle.xml", STATIC_DATA_PATH));
+
+        for(org.springframework.core.io.Resource patientFile : patientFileResources){
+            byte[] xmlString = IOUtils.toByteArray(patientFile.getInputStream());
+
+            HttpResponse response = post(xmlString);
+            System.out.println("File: " + patientFile.toString());
+            StatusLine sl = response.getStatusLine();
+            System.out.println("Status: " + sl.getStatusCode());
+            if (sl.getStatusCode() != 200) {
+                System.out.println("Cause: " + sl.toString());
+            }
+        }
+    }
+
+    protected static HttpResponse post(byte[] payload){
+        HttpPost postRequest = new HttpPost(RESOURCE_URI);
+        setRequestHeaders(postRequest);
+        return sendPayload(postRequest, payload);
+    }
+
+    protected static HttpResponse sendPayload(HttpEntityEnclosingRequestBase request, byte[] payload) {
+        HttpResponse response = null;
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            request.setEntity(new ByteArrayEntity(payload));
+            response = httpclient.execute(request);
+        } catch(IOException ioe) {
+            throw new RuntimeException("Error sending HTTP Post/Put Payload", ioe);
+        }
+        return response;
+    }
+
+    protected static void setRequestHeaders(HttpRequest request){
+        request.addHeader("User-Agent", "Java FHIR Client for FHIR");
+        request.addHeader("Accept", "application/fhir+json");
+        request.addHeader("Content-Type", "application/xml;charset=" + "UTF-8");
+        request.addHeader("Accept-Charset", "UTF-8");
+    }
+}
